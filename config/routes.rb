bcba310f1f5ce92f581eff87d63c2184d800b9a4
purpleice9765 <purpleice9765@msn.com>
2015-12-03 18:55:27 +0800

    fix routes bug, icon bugs

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :mains
 
 
-  root :to => 'mains#show'
+  root :to => 'mains#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
