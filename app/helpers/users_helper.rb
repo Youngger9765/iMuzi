ab5ded2c9c4be5bcef42d9897872a3d07be642a8
purpleice9765 <purpleice9765@msn.com>
2015-12-14 12:29:13 +0800

    refine alert bug when login

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,11 @@ module UsersHelper
   def teacher_comment_alert
     if current_user
       last_login_record = Impression.where("user_id=? AND controller_name=? AND action_name=?", current_user.id, "users","show").last
-      last_login_last_time = last_login_record.created_at
+      if last_login_record
+        last_login_last_time = last_login_record.created_at
+      else
+        last_login_last_time = nil
+      end
       teacher_comment_songs = current_user.songs.includes(:comments).where(:comments => {:status => "professional"})
       un_listen_songs = teacher_comment_songs.where("comments.created_at >= ?", last_login_last_time)
       un_listen_songs.count
