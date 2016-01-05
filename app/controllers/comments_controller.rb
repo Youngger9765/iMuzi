c97c168401b3f8846c226a0fbe54861edc3d654d
purpleice9765 <purpleice9765@msn.com>
2016-01-05 23:38:40 +0800

    comment.user can update comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -33,6 +33,33 @@ class CommentsController < ApplicationController
     redirect_to :back
   end
 
+  def update
+    @comment = Comment.find(params[:id])
+
+    if params[:destroy_logo] == "1"
+      @comment.logo = nil
+    end
+
+    if @comment.update(comment_params)
+      if comment_params[:link][0,32] == "https://www.youtube.com/watch?v="
+        @comment.link = comment_params[:link][32,100]
+      elsif comment_params[:link][0,32] != "https://www.youtube.com/watch?v="
+        @comment.link = nil
+      end
+      @comment.save!
+
+      flash[:success] = "更新成功!"
+      redirect_to song_path(@comment.song)
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+    @song = @comment.song
+  end
+
   private
 
   def comment_params
