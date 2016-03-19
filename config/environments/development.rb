e651eb2d9a9d4537273210abf8891136c79535eb
purpleice9765 <purpleice9765@msn.com>
2016-03-19 18:47:37 +0800

    try

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,5 +40,5 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 end
