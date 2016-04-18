3bcb9f158c2932daccbee4229f4718fb2f8707c2
purpleice9765 <purpleice9765@msn.com>
2016-04-18 23:16:42 +0800

    edit delete

--- app/controllers/admin/notifications_controller.rb
+++ app/controllers/admin/notifications_controller.rb
@@ -2,23 +2,31 @@ class Admin::NotificationsController < ApplicationController
 
   before_action :authenticate_user!
   before_action :check_admin
+  before_action :find_notification,  only: [:show, :edit, :update, :destroy]
   layout "admin"
 
   def index
-    @notifications = Notification.all
+    @notifications = Notification.all.order("created_at DESC")
+  end
+
+  def new
+    @notification = Notification.new
   end
 
   def create
-    
+    @notification = Notification.new(notification_params)
+    @notification.save
+    flash[:notice] = "推播成功!"    
+    redirect_to admin_notifications_path
+
   end
 
-  def show
-    
+  def destroy
+    flash[:success] = "刪除推播成功!" 
+    @notification.destroy
+    redirect_to admin_notifications_path
   end
 
-  def update
-    
-  end
 
   private
 
@@ -26,7 +34,15 @@ class Admin::NotificationsController < ApplicationController
     unless current_user.admin?
         raise ActiveRecord::RecordNotFound
     end
-  end 
+  end
+
+  def notification_params
+    params.require(:notification).permit( :user_id, :title, :content)
+  end
+
+  def find_notification
+    @notification = Notification.find(params[:id])
+  end
 
 
 end
