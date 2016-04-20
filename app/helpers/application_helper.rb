ccdf2b61ff7cc5d2eac26b3acf6d350617e437aa
purpleice9765 <purpleice9765@msn.com>
2016-04-20 09:54:47 +0800

    private notification display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,13 @@
 module ApplicationHelper
 
   def notifications 
-    Notification.where(:user_id => nil).order("created_at DESC").limit(5)
+    #notifications = Notification.where(:user_id => nil).order("created_at DESC").limit(5)
+
+    if current_user
+      user = current_user
+      notifications = Notification.where("user_id IS NULL || user_id =?", user.id).order("created_at DESC").limit(5)
+    else
+      notifications = Notification.where(:user_id => nil).order("created_at DESC").limit(5)
+    end
   end
 end
