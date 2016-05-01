7215f7e8a5c6b1af21f7a3314d4086c76e629d35
purpleice9765 <purpleice9765@msn.com>
2016-05-01 23:42:19 +0800

    reset back

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160430170042) do
+ActiveRecord::Schema.define(version: 20160430222632) do
 
   create_table "attachments", force: :cascade do |t|
     t.string   "picture",    limit: 255
